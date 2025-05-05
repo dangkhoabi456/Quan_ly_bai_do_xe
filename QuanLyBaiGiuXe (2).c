88377f32a29e2b151213f3c4d19b9b07de224486
DongTien <dongtien26pro@gmail.com>
2025-05-05 19:40:22 +0700

    fix loi hien thi thong ke tung tang

--- QuanLyBaiGiuXe (2).c
+++ QuanLyBaiGiuXe (2).c
@@ -831,5 +831,5 @@ int main(int argc, char **argv) {
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
-    return status;
+    return status;   
 }
